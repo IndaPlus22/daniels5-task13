8abb89f85a16a232455aa1807d547236b30d208c
danielsv03 <30444148+danielsv03@users.noreply.github.com>
2023-01-19 22:11:49 +0100

    Small fix

--- src/sphere.rs
+++ src/sphere.rs
@@ -13,19 +13,16 @@ impl Sphere {
     }
 }
 
-fn get_sphere_uv(p: Vec3) -> (f64, f64) {
-    let theta = (-1.0*p.y).acos();
-    let phi = f64::atan2(-1.0*p.z,p.x) + PI;
 
-    let u = phi / (2.0*PI);
-    let v = theta / PI;
-    return (u, v)
-
-
-}
 //Calulates if a given ray within a range of t_min and t_max for the t value hits the spehere, if so it returns a new Hitrecord for that hit.
 impl Hittable for Sphere {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        //The following code is solving for t in the following equation:
+        //t^2b*b + 2tb*(A-C)+(A-C)*(A-C)-r^2 = 0
+        //b is the direction of the ray
+        //A is the start position of the ray
+        //C is the center of the sphere
+        //r is the radius of the circle
         let oc = r.origin() - self.center;
         let a = r.direction().squared_length();
         let half_b = Vec3::dot(oc, r.direction());
@@ -34,12 +31,14 @@ impl Hittable for Sphere {
         let discriminant = half_b*half_b - a*c;
         let sqrtd = f64::sqrt(discriminant);
         let mut root = (-half_b - sqrtd) / a;
+        //Check for the first root / first hit point
         if(root > t_min && t_max > root) {
             let mut rec = HitRecord::new(r.at(root), root);
             rec.mat = self.mat;
             let outward_normal = (rec.p - self.center) / self.radius;
             return Some(rec.set_face_normal(*r, outward_normal));
         }
+        //Check if the second root / hit point is within the constraints
         root = (-half_b + sqrtd) / a;
         if root > t_min && t_max > root {
             let mut rec = HitRecord::new(r.at(root), root);
